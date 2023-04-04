00b3884c740992686ec9161dd1e8495eb907c178
indihth <86363851+indihth@users.noreply.github.com>
2023-04-04 15:59:11 +0100

    edited routes to include admin and user specific controllers

--- Laravel/Trkr/routes/web.php
+++ Laravel/Trkr/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-use App\Http\Controllers\GoalController;
+use App\Http\Controllers\admin\GoalController as AdminGoalController;
+use App\Http\Controllers\admin\TaskController as AdminTaskController;
+use App\Http\Controllers\user\GoalController as UserGoalController;
+
+// use App\Http\Controllers\GoalController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,9 +28,10 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Allows only logged in authorised users to access the tvshows page
-Route::resource('/goals', GoalController::class)->middleware(['auth']);
+Route::resource('/admin/goals', AdminGoalController::class)->middleware(['auth'])->names('admin.goals');
 Route::resource('/tasks', TaskController::class)->middleware(['auth'])->except('create, edit');
 
-Route::get('/tasks/{id}/create', [App\Http\Controllers\TaskController::class, 'create'])->name('tasks.create');
-Route::get('/tasks/{id}/edit', [App\Http\Controllers\TaskController::class, 'edit'])->name('tasks.edit');
+// Routes created seperately in order to pass the goal id through to the task create and edit views
+Route::get('/admin/tasks/{id}/create', [App\Http\Controllers\AdminTaskController::class, 'create'])->name('tasks.create');
+Route::get('/admin/tasks/{id}/edit', [App\Http\Controllers\AdminTaskController::class, 'edit'])->name('tasks.edit');
 
